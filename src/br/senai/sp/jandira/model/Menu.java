9cc6e49f01f69438adc1df224f3e831c82b613a4
Ana Luiza <analuiza.azevedomorais@gmail.com>
2023-09-13 17:25:35 -0300

    falta terminar

--- src/br/senai/sp/jandira/model/Menu.java
+++ src/br/senai/sp/jandira/model/Menu.java
@@ -6,26 +6,45 @@ public class Menu {
 
     //Essa classe vai perguntar o que o usuário quer fazer
 
-    Tarefas tarefas = new Tarefas();
+    Tarefas tarefaObj = new Tarefas();
     Scanner teclado = new Scanner(System.in);
     String escolha;
     public void Menu(){
 
         while (true) {
 
+
             System.out.println("Olá, o que deseja fazer??");
-            System.out.println("Listar tarefas [1] - Criar novas tarefas [2] -\n Atualizar tarefas [3] - Remover tarefas [4]");
+            System.out.println("Criar novas tarefas[1] - Listar tarefas  [2] ");
+            System.out.println(" Atualizar tarefas [3] - Remover tarefas [4] ");
+            System.out.println("Marcar como concluída ou pendente [5] - Exit [6]");
             escolha = teclado.nextLine();
+            System.out.println("");
 
             if (escolha.equalsIgnoreCase("1")) {
-                tarefas.listarTarefas();
+
+                Tarefas tarefa = tarefaObj.informacoesTarefas();
+                tarefaObj.adicionarTarefa(tarefa);
+                System.out.println("");
+
             } else if (escolha.equalsIgnoreCase("2")) {
-                tarefas.informacoesTarefas(tarefas);
+
+                tarefaObj.listarTarefas();
+                System.out.println("");
 
             } else if (escolha.equalsIgnoreCase("3")) {
 
+                System.out.println("");
+
             } else if (escolha.equalsIgnoreCase("4")) {
 
+                tarefaObj.removerTarefas();
+                System.out.println("");
+
+            } else if (escolha.equalsIgnoreCase("5")) {
+                tarefaObj.concluidas();
+            } else if (escolha.equalsIgnoreCase("6")) {
+                break;
             }
 
         }
--- src/br/senai/sp/jandira/model/Tarefas.java
+++ src/br/senai/sp/jandira/model/Tarefas.java
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Tarefas {
 
@@ -13,33 +14,104 @@ public class Tarefas {
 
   Scanner teclado = new Scanner(System.in);
   String nome, dataCriacao, prazo;
+  int tamanhoLista;
+  String finalizada;
 
-  List<Tarefas> tarefas = new ArrayList<>();
 
-  public void informacoesTarefas(Tarefas tarefa){
+  List<Tarefas> listaTarefas = new ArrayList<>();
 
+  public Tarefas informacoesTarefas(){
+
+    Tarefas tarefas = new Tarefas();
     System.out.println("Qual o nome da sua tarefa?");
-      nome = teclado.nextLine();
+     tarefas.nome = teclado.nextLine();
+      System.out.println("");
 
     System.out.println("Qual é a data de hoje?");
-    dataCriacao = teclado.nextLine();
+    tarefas.dataCriacao = teclado.nextLine();
+      System.out.println("");
 
     System.out.println("Para quando é a entrega?");
-    prazo = teclado.nextLine();
+    tarefas.prazo = teclado.nextLine();
 
-    tarefas.add(tarefa);
+      System.out.println("");
 
+      System.out.println("Essa tarefa está pendente ou já foi concluída? [C] - concluída [P] - pendente");
+      finalizada = teclado.nextLine();
+
+return tarefas;
     }
-
+//---------------------------------------------------------------------------------------
+    public void adicionarTarefa(Tarefas tarefa){
+      listaTarefas.add(tarefa);
+      tamanhoLista = listaTarefas.size();
+    }
+//---------------------------------------------------------------------------------------
     //Nessa parte ele lista as tarefas e adiciona elas na lista
 
 
     public void listarTarefas(){
 
-      for (Tarefas tarefa : tarefas){
+      for (Tarefas tarefa : listaTarefas){
         System.out.println(tarefa.nome);
 
       }
     }
 
+//---------------------------------------------------------------------------------------
+    public void removerTarefas(){
+
+
+      System.out.println("Escolha o item que deseja excluir: [1,2,3,4...]");
+      System.out.println("Caso queira excluir tudo digite 0");
+      int excluir = teclado.nextInt();
+
+      if (excluir == 0){
+        listaTarefas.clear();
+
+    } else if (excluir <= tamanhoLista) {
+
+        listaTarefas.remove(excluir - 1);
+
+      } else {
+
+        System.out.println("Não foi encontrado esse item na lista");
+      }
+
+    }
+
+//---------------------------------------------------------------------------------------
+
+public void editarTarefas(int indice){
+
+    System.out.println("Qual tarefa deseja editar?");
+
+
 }
+
+//---------------------------------------------------------------------------------------
+
+public void concluidas(){
+
+//    System.out.println("Para marcar como concluída digite [C]");
+//    String respostaConclusao = teclado.nextLine();
+//
+//    System.out.println("");
+//
+//    System.out.println("Para marcar como pendente digite [P]");
+//    String respostaPendente = teclado.nextLine();
+
+   if (finalizada == "C"){
+       System.out.println(nome + " - Concluída");
+
+   } else if (finalizada == "P") {
+       System.out.println(nome + "Pendente");
+
+   }
+
+
+}
+}
+
+
+
